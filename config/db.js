4f396df6e199f624594941f1fb091cc5cc1d7c28
MadhavR97 <rathodmadhav215@gmail.com>
2026-01-19 13:07:02 +0530

    Change URL Local to MongoDB Atlas

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const local = true;
+    const local = false;
 
     if (local) {
       await mongoose.connect(process.env.LOCAL_MONGODB_URL);
