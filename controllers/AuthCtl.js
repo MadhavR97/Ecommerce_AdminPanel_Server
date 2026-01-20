06d4c21c64b9813e0e5923a94f0a362c60d41edd
MadhavR97 <rathodmadhav215@gmail.com>
2026-01-20 11:35:43 +0530

    Fix Variable 'roll' to 'role'

--- controllers/AuthCtl.js
+++ controllers/AuthCtl.js
@@ -57,7 +57,7 @@ module.exports.DeleteUser = async (req, res) => {
 // Get Admin / User
 module.exports.LoginUser = async (req, res) => {
     try {
-        const existUser = await userSchema.findOne({ roll: req.body.roll, email: req.body.email })
+        const existUser = await userSchema.findOne({ role: req.body.role, email: req.body.email })
 
         if (!existUser) {
             return res.status(500).json({ message: 'User not exist' })
@@ -79,7 +79,7 @@ module.exports.LoginUser = async (req, res) => {
 // Get Admin / User
 module.exports.GetUsers = async (req, res) => {
     try {
-        const users = await userSchema.find({ roll: 'user' });
+        const users = await userSchema.find({ role: 'user' });
         res.status(200).json({ users });
     }
     catch (error) {
